a15d9367e23d363c0c8be3959c27b5c5b1878b17
Dmitry <d.dismyt@ya.ru>
2023-03-04 19:47:25 +0300

    custom longpolling

--- bot.py
+++ bot.py
@@ -1,22 +1,40 @@
-from aiogram import Bot, types
-from aiogram.dispatcher import Dispatcher
-from aiogram.utils import executor
+"""
+ который получает сообщения, проверяет новое ли оно и если новое,
+ а также если это написал пользователь-администратор и текст равен /start,
+ он будет отсылать нам обратно сообщение с Привет username
+"""
+import time
+
+import requests
 
 from config import TOKEN
 
-bot = Bot(token=TOKEN)
-dp = Dispatcher(bot)
+BASE_URL = f'https://api.telegram.org/bot{TOKEN}'
+ADMIN = [381762408, ]  # Тут ваш id
 
 
-@dp.message_handler(commands="test1")
-async def cmd_test1(message: types.Message):
-    await message.reply("Test 1")
+def polling():
+    count_msg = 0
+    while True:
+        time.sleep(1)
+        response = requests.get(f'{BASE_URL}/getUpdates').json()
+        if count_msg != len(response['result']):
+            count_msg = len(response['result'])
+            print(count_msg)
+
+            msg = response['result'][-1]['message']
+            user_id = msg['from']['id']
+            chat_id = msg['chat']['id']
+
+            if user_id in ADMIN:
+                print('--- сработало ---')
+                headers = {'content-type': 'multipart/form-data'}
+                file_id = msg['sticker']['file_id']
+                params = {
+                    'chat_id': chat_id,
+                    'sticker': file_id,
+                }
+                requests.post(f'{BASE_URL}/sendSticker', params=params, headers=headers)
 
 
-@dp.message_handler()
-async def echo_message(msg: types.Message):
-    await bot.send_message(msg.from_user.id, '213')
-
-
-if __name__ == '__main__':
-    executor.start_polling(dp)
+polling()
