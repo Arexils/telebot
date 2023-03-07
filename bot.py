a9abaaa9905fdb9183690ab5d85b33f1493e9914
Dmitry <d.dismyt@ya.ru>
2023-03-07 21:54:44 +0300

    Клавиатуры

--- bot.py
+++ bot.py
@@ -1,40 +1,51 @@
 """
- который получает сообщения, проверяет новое ли оно и если новое,
- а также если это написал пользователь-администратор и текст равен /start,
- он будет отсылать нам обратно сообщение с Привет username
+Реализовать эхо бота с использование Telegram bot api. Бот должен не только принимать сообщения, но и изображения
 """
-import time
-
 import requests
 
 from config import TOKEN
 
 BASE_URL = f'https://api.telegram.org/bot{TOKEN}'
-ADMIN = [381762408, ]  # Тут ваш id
+ADMINS = [381762408, ]  # Тут ваш id
 
 
-def polling():
-    count_msg = 0
+def pulling():
+    count_message = 0
     while True:
-        time.sleep(1)
         response = requests.get(f'{BASE_URL}/getUpdates').json()
-        if count_msg != len(response['result']):
-            count_msg = len(response['result'])
-            print(count_msg)
+        if count_message != len(response['result']):
+            count_message = len(response['result'])
+            try:
+                message = response['result'][-1]['message']
+                chat_id = message['chat']['id']
 
-            msg = response['result'][-1]['message']
-            user_id = msg['from']['id']
-            chat_id = msg['chat']['id']
-
-            if user_id in ADMIN:
-                print('--- сработало ---')
-                headers = {'content-type': 'multipart/form-data'}
-                file_id = msg['sticker']['file_id']
-                params = {
+                json_keyboard = {
+                    'method': 'sendMessage',
                     'chat_id': chat_id,
-                    'sticker': file_id,
+                    'text': 'Какая-то клавиатура',
+                    'reply_markup': {
+
+                        'keyboard': [
+                            [
+                                {
+                                    'text': 'YeaS',
+                                },
+                                {
+                                    'text': 'No!',
+                                },
+                            ],
+                            [
+                                'Ряд 2',
+                            ],
+
+                        ]
+                    },
+                    'resize_keyboard': True,
+                    'one_time_keyboard': False,
                 }
-                requests.post(f'{BASE_URL}/sendSticker', params=params, headers=headers)
+                requests.post(f'{BASE_URL}/', json=json_keyboard)
+            except Exception as error:
+                print(f'Случилась ошибка: {error}')
 
 
-polling()
+pulling()
