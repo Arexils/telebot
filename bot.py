4411276b79f81a8facee9011f1dd888c8a5d24e0
Dmitry <d.dismyt@ya.ru>
2023-03-18 18:14:49 +0300

    бот с инфо

--- bot.py
+++ bot.py
@@ -1,55 +1,50 @@
 import logging
 
 from aiogram import Bot, Dispatcher, types
-from aiogram.contrib.middlewares.logging import LoggingMiddleware
-from aiogram.dispatcher import webhook
 
-from config import TOKEN, WEBHOOK_URL, WEBHOOK_PATH, WEBAPP_HOST, WEBAPP_PORT
+from config import TOKEN
 
 logging.basicConfig(level=logging.INFO)
+
 bot = Bot(
     token=TOKEN,
-    parse_mode='HTML',
+    parse_mode='HTML'
 )
 dp = Dispatcher(bot)
-dp.middleware.setup(LoggingMiddleware())
 
 
 async def on_startup(dp):
-    logging.info('Setting webhook :D')
-    await bot.set_webhook(WEBHOOK_URL)
+    logging.info('new start')
 
 
-async def on_shutdown(dp):
-    logging.info('Shutting down..')
-    await bot.delete_webhook()
-    logging.info('Bye!')
+async def on_shotdown(dp):
+    logging.info('end start')
 
 
-@dp.message_handler(commands=['start'])
-async def command_start(msg: types.Message):
-    return webhook.SendMessage(msg.chat.id, 'Добро пожаловать!')
+@dp.message_handler(
+    text='пакет',
+    content_types=types.ContentType.TEXT,
+)
+async def echo(msg: types.Message):
+    await msg.answer(msg.text)
 
 
-@dp.message_handler(commands=['help'])
-async def command_help(msg: types.Message):
-    return webhook.SendMessage(msg.chat.id, 'Вы обратились к справке бота')
-
-
-@dp.message_handler(commands=['myID'])
-async def get_id_user(msg: types.Message):
-    return webhook.SendMessage(msg.chat.id, f'ID: <code>{msg.chat.id}</code>')
+@dp.message_handler(commands=['info'])
+async def command_info_profile(msg: types.Message):
+    text = (
+        f'{msg.from_user.mention}\n'
+        f'<b>ID: </b> <code>{msg.from_user.id}</code>\n'
+        f'<b>Chat ID: </b> {msg.chat.id}\n'
+    )
+    await msg.answer(text)
 
 
 if __name__ == '__main__':
     from aiogram import executor
 
-    executor.start_webhook(
+    executor.start_polling(
         dispatcher=dp,
         skip_updates=True,
         on_startup=on_startup,
-        on_shutdown=on_shutdown,
-        webhook_path=WEBHOOK_PATH,
-        host=WEBAPP_HOST,
-        port=WEBAPP_PORT,
+        on_shutdown=on_shotdown,
     )
