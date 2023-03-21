03bd4aa592fc20f6d653406c026d9495fb94c328
Dmitry <d.dismyt@ya.ru>
2023-03-21 20:55:35 +0300

    [L] кнб

--- bot.py
+++ bot.py
@@ -1,17 +1,22 @@
 import logging
 
-import requests
 from aiogram import Bot, Dispatcher, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters.state import StatesGroup, State
 
 from config import TOKEN
+from utils import knd_logic
+
+bot = Bot(token=TOKEN)
+storage = MemoryStorage()
+dp = Dispatcher(bot, storage=storage)
 
 logging.basicConfig(level=logging.INFO)
 
-bot = Bot(
-    token=TOKEN,
-    parse_mode='HTML'
-)
-dp = Dispatcher(bot)
+
+class Form(StatesGroup):
+    name = State()
 
 
 async def on_startup(dp):
@@ -22,28 +27,37 @@ async def on_shotdown(dp):
     logging.info('end start')
 
 
-@dp.message_handler(
-    text='пакет',
-    content_types=types.ContentType.TEXT,
-)
-async def echo(msg: types.Message):
-    await msg.answer(msg.text)
+@dp.message_handler(commands=['knb'])
+async def knb(message: types.Message):
+    """Conversation entrypoint"""
+
+    # Set state
+    await Form.name.set()
+    await message.reply("Send me your name")
 
 
-@dp.message_handler(commands=['info'])
-async def command_info_profile(msg: types.Message):
-    text = (
-        f'{msg.from_user.mention}\n'
-        f'<b>ID: </b> <code>{msg.from_user.id}</code>\n'
-        f'<b>Chat ID: </b> {msg.chat.id}\n'
-    )
-    await msg.answer(text)
+# You can use state='*' if you want to handle all states
+@dp.message_handler(state='*', commands=['cancel'])
+async def cancel_handler(message: types.Message, state: FSMContext):
+    """Allow user to cancel action via /cancel command"""
+
+    current_state = await state.get_state()
+    if current_state is None:
+        # User is not in any state, ignoring
+        return
+
+    # Cancel state and inform user about it
+    await state.finish()
+    await message.reply('Cancelled.')
 
 
-@dp.message_handler(commands=['funny'])
-async def command_funny(msg: types.Message):
-    response = requests.get('https://aws.random.cat/meow').json().get('file')
-    await msg.answer_photo(response)
+@dp.message_handler(state=Form.name)
+async def process_name(message: types.Message, state: FSMContext):
+    """Process user name"""
+
+    # Finish our conversation
+    await state.finish()
+    await message.reply(f"Hello, {knd_logic(message.text)}")  # <-- Here we get the name
 
 
 if __name__ == '__main__':
---
+++ utils.py
@@ -0,0 +1,35 @@
+import logging
+from random import choice
+
+
+# TODO: Для доп ДЗ необходимо оптимизировать данный код
+def knd_logic(user_selection):
+    RPS = ['камень', 'ножницы', 'бумага']
+
+    if user_selection.lower() in RPS:
+        bot_selection = choice(RPS)  # выбор бота
+
+        logging.info(f'бот: {bot_selection}')
+
+        if user_selection == "камень" and bot_selection == "камень":
+            result = "ничья"
+        elif user_selection == "камень" and bot_selection == "ножницы":
+            result = "пользователь"
+        elif user_selection == "камень" and bot_selection == "бумага":
+            result = "бот"
+
+        if user_selection == "ножницы" and bot_selection == "камень":
+            result = "бот"
+        elif user_selection == "ножницы" and bot_selection == "ножницы":
+            result = "ничья"
+        elif user_selection == "ножницы" and bot_selection == "бумага":
+            result = "пользователь"
+
+        if user_selection == "бумага" and bot_selection == "камень":
+            result = "пользователь"
+        elif user_selection == "бумага" and bot_selection == "ножницы":
+            result = "бот"
+        elif user_selection == "бумага" and bot_selection == "бумага":
+            result = "ничья"
+        return result
+    return None
